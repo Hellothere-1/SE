1fffdfe648b719d34fbbe5b0da32b4b4c713a35f
LWort <LWort@DESKTOP-JT3MPG0>
2017-09-15 14:26:21 +0200

    Version 0

---
+++ Gravity Capture/Program.cs
@@ -0,0 +1,57 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        List<IMyGravityGeneratorBase> FrBa = new List<IMyGravityGeneratorBase>();
+        List<IMyGravityGeneratorBase> LeRi = new List<IMyGravityGeneratorBase>();
+        List<IMyGravityGeneratorBase> UpDo = new List<IMyGravityGeneratorBase>();
+        List<IMyGravityGeneratorBase> Shield = new List<IMyGravityGeneratorBase>();
+        List<IMyGravityGeneratorBase> Rota1 = new List<IMyGravityGeneratorBase>();
+        List<IMyGravityGeneratorBase> Rota2 = new List<IMyGravityGeneratorBase>();
+
+        public Program()
+        {
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked.
+            // 
+            // The method itself is required, but the argument above
+            // can be removed if not needed.
+        }
+    }
+}
